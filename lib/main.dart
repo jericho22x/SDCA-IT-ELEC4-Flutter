061fce8e536d39631434ca4b36234e4986628962
Jericho Tibayan <tyunako@gmail.com>
2025-08-30 18:20:42 +0800

    Initial FLutter Commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text("Hello World, puro hello trabaho ayaw?"));
+  }
+}
