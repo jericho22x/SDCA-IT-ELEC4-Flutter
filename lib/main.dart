d79d195fc204c0a1cbe706aef66a839ab32fe8bb
Jericho Tibayan <tyunako@gmail.com>
2025-08-30 18:24:03 +0800

    Initial Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Hello World, puro hello trabaho ayaw?"));
+    Scaffold homepage = Scaffold(appBar: AppBar(title: Text('Homepage')));
+
+    return MaterialApp(
+      home: homepage,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: const Color.fromARGB(255, 228, 22, 22),
+          foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+        ),
+      ),
+    );
   }
 }
